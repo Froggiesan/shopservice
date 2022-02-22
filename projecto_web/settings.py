7c48ad99a3a275b1bc339e6a9b727ec3f0e56ff1
Froggiesan <77341642+Froggiesan@users.noreply.github.com>
2022-02-22 13:22:32 +0100

    create app servicios and model

--- projecto_web/settings.py
+++ projecto_web/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'projecto_web_app'
+    'projecto_web_app',
+    'servicios',
 ]
 
 MIDDLEWARE = [
